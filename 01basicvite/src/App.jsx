a3757b4d17d3895a6ff15dffbbb9df060204f9c5
shiori0208 <shioriuem02@gmail.com>
2025-08-30 23:23:31 +0530

    adding evaluated expressions to App.js...

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,11 +1,11 @@
 import Youtube from "./Youtube"
 
 function App() {
-
+const username = "shiori"
   return (
     <>
-    <h1>Vite React App</h1>
-    <h1>Vite React App</h1>
+    <h1>Vite React App {2+2}</h1>
+    <h1>Vite React App {username}</h1>
     <Youtube />
     </>
   )
