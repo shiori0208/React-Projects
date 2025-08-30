5f89599d24f1fe082e04e0a9cdc8fffd61bd3d7f
shiori0208 <shioriuem02@gmail.com>
2025-08-31 03:53:03 +0530

    making a counter using React...

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15) 
+  
+  //first val variable, second val function 
+  //let counter = 15 
+
+  const addValue = () => {
+    setCounter((prevCount) => prevCount + 1)
+    setCounter((prevCount) => prevCount + 1)
+    setCounter((prevCount) => prevCount + 1)
+    setCounter((prevCount) => prevCount + 1)
+  }; 
+
+  const minusValue = () => {
+    setCounter(counter - 1)
+  }; 
+
+  //called variable injection
+
+  return (
+  <>
+  <h1>React learnin with Chi</h1> 
+  <h2>Counter value: {counter} </h2>
+  <button onClick={addValue}>+</button>
+  <button onClick={minusValue}>-</button>
+  <p>Footer: </p>
+    </>
+  )
+}
+
+export default App
