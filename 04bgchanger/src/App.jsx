57c7bdd3fc8e2e5ce1fffac7ce60016eab436484
shiori0208 <shioriuem02@gmail.com>
2025-09-01 01:31:53 +0530

    debugging and making buttons for color changes...

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,31 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen duration-200 bg-amber-600'>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-amber-300 px-3 py-2 rounded-3xl'>
+          <button>Summer</button>
+          <button>Monsoon</button>
+          <button>Autumn</button>
+          <button>Spring</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
