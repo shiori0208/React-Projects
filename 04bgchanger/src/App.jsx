87a990eae85305251a3ebedd2816e6004bd1984e
shiori0208 <shioriuem02@gmail.com>
2025-09-01 22:29:56 +0530

    adding button customizations and default bg...

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,16 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const [color, setColor ] = useState('black'); 
 
   return (
-    <div className='w-full h-screen duration-200 bg-amber-600'>
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-amber-300 px-3 py-2 rounded-3xl'>
-          <button>Summer</button>
-          <button>Monsoon</button>
-          <button>Autumn</button>
-          <button>Spring</button>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-gray-300 px-3 py-2 rounded-3xl'>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg bg-yellow-500 text-black'>Summer</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-800 text-white'>Monsoon</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg bg-amber-600 text-black'>Autumn</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-300 text-black'>Winter</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg bg-pink-300 text-black'>Spring</button>
         </div>
       </div>
     </div>
