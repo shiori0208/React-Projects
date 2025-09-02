04b3d928a4058eb1e4879fc29905343a86c83673
shiori0208 <shioriuem02@gmail.com>
2025-09-02 23:34:41 +0530

    successfully changed bg colors...

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -6,19 +6,19 @@ import './App.css'
 function App() {
   const [color, setColor ] = useState('black'); 
 
-  function changeColor() {
-
+  function changeColor(color) {
+    setColor(color)
   }
 
   return (
     <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-gray-300 px-3 py-2 rounded-3xl'>
-          <button onClick={changeColor} className='outline-none px-4 py-1 rounded-full shadow-lg bg-yellow-500 text-black'>Summer</button>
-          <button onClick={changeColor} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-800 text-white'>Monsoon</button>
-          <button onClick={changeColor} className='outline-none px-4 py-1 rounded-full shadow-lg bg-amber-600 text-black'>Autumn</button>
-          <button onClick={changeColor} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-300 text-black'>Winter</button>
-          <button onClick={changeColor} className='outline-none px-4 py-1 rounded-full shadow-lg bg-pink-300 text-black'>Spring</button>
+          <button onClick={() => changeColor('yellow')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-yellow-500 text-black'>Summer</button>
+          <button onClick={() => changeColor('blue')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-800 text-white'>Monsoon</button>
+          <button onClick={() => changeColor('orange')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-amber-600 text-black'>Autumn</button>
+          <button onClick={() => changeColor('aqua')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-300 text-black'>Winter</button>
+          <button onClick={() => changeColor('pink')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-pink-300 text-black'>Spring</button>
         </div>
       </div>
     </div>
