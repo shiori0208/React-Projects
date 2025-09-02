d26db5645008744afa6ca3ed7ae52ff69b59f1f8
shiori0208 <shioriuem02@gmail.com>
2025-09-03 00:35:12 +0530

    moving pictures in bg...

--- 04bgchanger/src/App2.jsx
+++ 04bgchanger/src/App2.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import summerImg from './images/summer.jpg'
@@ -13,10 +11,9 @@ import springImg from './images/spring.jpg'
 function App2() {
   const [bg, setBg] = useState({
     color: 'black',
-    image: ''
+    image: '',
+    positionX: 'left'
   });
-
-  // Example image URLs (replace with your own images)
 
   return (
     <div
@@ -24,39 +21,39 @@ function App2() {
       style={{
         backgroundColor: bg.color,
         backgroundImage: bg.image ? `url(${bg.image})` : 'none',
-        backgroundSize: 'auto',
-        backgroundPosition: 'center',
+        backgroundSize: '500px 1000px',
+        backgroundPosition: `${bg.positionX} center`,
         backgroundRepeat: 'no-repeat'
       }}
     >
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-gray-300 px-3 py-2 rounded-3xl'>
-          <button
-            onClick={() => setBg({ color: '#F7DC9F', image: summerImg })}
+           <button
+            onClick={() => setBg({ color: '#F7DC9F', image: summerImg, positionX: 'left' })}
             className='outline-none px-4 py-1 rounded-full shadow-lg bg-yellow-500 text-black'
           >
             Summer
           </button>
           <button
-            onClick={() => setBg({ color: '#4C9DB0', image: monsoonImg })}
+            onClick={() => setBg({ color: '#4C9DB0', image: monsoonImg, positionX: '232.4px' })}
             className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-800 text-white'
           >
             Monsoon
           </button>
           <button
-            onClick={() => setBg({ color: 'orange', image: autumnImg })}
+            onClick={() => setBg({ color: 'orange', image: autumnImg, positionX: '520.8px' })}
             className='outline-none px-4 py-1 rounded-full shadow-lg bg-amber-600 text-black'
           >
             Autumn
           </button>
           <button
-            onClick={() => setBg({ color: '#BDD7DE', image: winterImg })}
+            onClick={() => setBg({ color: '#BDD7DE', image: winterImg, positionX: '697.6px' })}
             className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-300 text-black'
           >
             Winter
           </button>
           <button
-            onClick={() => setBg({ color: 'pink', image: springImg })}
+            onClick={() => setBg({ color: 'pink', image: springImg, positionX: '1088px' })}
             className='outline-none px-4 py-1 rounded-full shadow-lg bg-pink-300 text-black'
           >
             Spring
