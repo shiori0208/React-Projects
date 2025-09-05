134738f5bd0e57c53e4124c5053113186be0178a
shiori0208 <shioriuem02@gmail.com>
2025-09-06 01:38:28 +0530

    imported and added router elements...

--- 07reactrouter/src/Layout.jsx
+++ 07reactrouter/src/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
-
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function Layout() {
   return (
-    <div>Layout</div>
+    <>
+    <Header />
+    <Outlet /> //makes layout design possible. makes sure header and footer are on all pages
+    <Footer />
+    </>
   )
 }
 
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+
+      </Route>
+    )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
