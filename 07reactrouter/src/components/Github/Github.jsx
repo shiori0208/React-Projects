c2c824a61b720d08e985573fc11fb8f7d0ea98cd
shiori0208 <shioriuem02@gmail.com>
2025-09-06 18:13:37 +0530

    added all given components and routed them accurately...

--- 07reactrouter/src/components/Github/Github.jsx
+++ 07reactrouter/src/components/Github/Github.jsx
@@ -4,29 +4,23 @@ import {useLoaderData} from 'react-router-dom'
 
 function Github() {
 
-    const data = useLoaderData()
-
-    // const [data, setData] = React.useState([])
-    // useEffect(() => {
-    //     fetch('https://api.github.com/users/hiteshchoudhary')
-    //     .then((response) => response.json())
-    //     .then(data => {
-    //         console.log(data)
-    //         setData(data)
-    //     })
-    // }, [])
+  const [data, setData] = React.useState([])
+  useEffect( () => {
+    fetch('https://api.github.com/users/shiori0208')
+    .then((response) => response.json())
+    .then(data=> {
+      console.log(data)
+      setData(data)
+    })
+  }, [])
 
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
-    <img src={data.avatar_url} width={300} alt="" />
+    <div className='flex flex-col items-center text-center m-4 bg-amber-500 text-white p-4 text-3xl'>
+      <img className='mx-auto' src={data.avatar_url} width={300} alt="" />
+      Github Followers: {data.followers}
     </div>
   )
+
 }
 
 export default Github
-
-
-export const githubInfoLoader = async () => {
-    const response = await fetch('https://api.github.com/users/shiori0208')
-    return response.json()
-}
