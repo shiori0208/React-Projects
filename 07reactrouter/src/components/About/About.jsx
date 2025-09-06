26e8301666c987cc24c98ecb753de3f3a35794fa
shiori0208 <shioriuem02@gmail.com>
2025-09-06 14:23:45 +0530

    added workinf image urls...

--- 07reactrouter/src/components/About/About.jsx
+++ 07reactrouter/src/components/About/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://i.pinimg.com/1200x/12/e0/19/12e019cf7db26dcaaea7e88c49be27cc.jpg"
                             alt="image"
                         />
                     </div>
--- 07reactrouter/src/components/Home/Home.jsx
+++ 07reactrouter/src/components/Home/Home.jsx
@@ -32,12 +32,12 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://blush.design/illustration/i/aUDFbga5P30IHv1UoMIm" alt="image1" />
+                    <img className="w-96" src="https://i.pinimg.com/736x/24/83/3d/24833da4c0974bfd71175c92c9d75d6c.jpg" alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src="https://i.pinimg.com/736x/af/a5/7f/afa57ffb8a4086cc79db618054e0244f.jpg" alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
