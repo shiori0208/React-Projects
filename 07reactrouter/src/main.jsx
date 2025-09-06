4f35db71db48b5226b46205d37c32b64dcd47980
shiori0208 <shioriuem02@gmail.com>
2025-09-06 17:47:46 +0530

    added user and github routes in main...

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -6,15 +6,23 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} f
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Github from './components/Github/Github.jsx'
+import User from './components/User/User.jsx'
 
 //home and about have same hierarchy after /
 //if we want to add more stuff within a route make another route tag inside and continue
+//the ':' makes the word next to it go into userparam hook 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout />} >
         <Route path='' element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="user/" element={<User />}>
+          <Route path=':userid' element={<User />} />
+        </Route>
+        <Route path="github" element={<Github />}/>
+        <Route path='*' element={<div>Not found</div>} />
       </Route>
     )
 )
