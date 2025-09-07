54f9a755d53843da96aadd2eda810f66cdb1608d
shiori0208 <shioriuem02@gmail.com>
2025-09-07 18:15:43 +0530

    added context providers after designing them...

--- 08minicontextapi/src/components/Login.jsx
+++ 08minicontextapi/src/components/Login.jsx
@@ -15,8 +15,16 @@ function Login() {
         <h2> Login </h2>
         <input 
         type="text" 
+        value={username}
         onChange={(e) => setUsername(e.target.value)}
+        placeholder = 'username' />
+        {" "}
+        <input type="password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='password'
         />
+        <button onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
