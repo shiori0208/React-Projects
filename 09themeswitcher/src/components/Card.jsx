e92e5844c12dac4c2a4a48072dc81cd829fad368
shiori0208 <shioriuem02@gmail.com>
2025-09-08 22:43:30 +0530

    trying to debug components...

--- 09themeswitcher/src/components/Card.jsx
+++ 09themeswitcher/src/components/Card.jsx
@@ -4,12 +4,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://i.pinimg.com/736x/6d/81/e7/6d81e7df501707a352c5467633f994fb.jpg" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://i.pinimg.com/736x/c6/29/23/c629236b18c4a5c293598b83b51900c3.jpg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Wall-E and Eva Poster 24x24 inch HD 
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -63,7 +63,7 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$12</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
