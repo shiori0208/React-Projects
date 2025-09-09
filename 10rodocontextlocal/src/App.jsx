398dd165a58758866e7601cd78108d4900106a73
shiori0208 <shioriuem02@gmail.com>
2025-09-09 23:16:09 +0530

    adding context to app..

--- 10rodocontextlocal/src/App.jsx
+++ 10rodocontextlocal/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ToDoProvider } from './contexts/ToDoContexts'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ToDoProvider>
      <h1 className='text-3xl font-bold underline bg-amber-300'>OOF</h1>
-    </>
+    </ToDoProvider>
   )
 }
 
