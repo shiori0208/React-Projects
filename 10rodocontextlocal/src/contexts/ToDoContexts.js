dfb05478b6135b8d41c32e4d4b38dcd1816fc7af
shiori0208 <shioriuem02@gmail.com>
2025-09-09 23:07:59 +0530

    making ToDoContext component...

--- 10rodocontextlocal/src/contexts/ToDoContexts.js
+++ 10rodocontextlocal/src/contexts/ToDoContexts.js
@@ -1,4 +1,24 @@
 import {createContext, useContext} from "react"
 
 
-export const ToDoContext = createContext({})
+export const ToDoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "ToDo Message",
+            completed: false
+        }
+    ], 
+
+    addToDo: (todo) => {}, 
+    updateToDo: (id, todo) => {},
+    deleteToDo: (id) => {},
+    toggleComplete: () => {}
+
+})
+
+export const useToDo = () => {
+    return useContext(ToDoContext)
+}
+
+export const ToDoProvider = ToDoContext.Provider 
