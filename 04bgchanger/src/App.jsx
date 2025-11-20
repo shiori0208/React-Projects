cf7a2ba316ba3f3b80647c4697bc46f7d76dfcc6
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-11-20 23:40:06 +0530

    Update App.jsx

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-gray-300 px-3 py-2 rounded-3xl'>
           <button onClick={() => setColor('#F7DC9F')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-yellow-400 text-black'>Summer</button>
-          <button onClick={() => setColor('#4C9DB0')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-800 text-white'>Monsoon</button>
+          <button onClick={() => setColor('#4C9DB0')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-700 text-white'>Monsoon</button>
           <button onClick={() => setColor('orange')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-amber-600 text-black'>Autumn</button>
           <button onClick={() => setColor('#BDD7DE')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-blue-300 text-black'>Winter</button>
           <button onClick={() => setColor('pink')} className='outline-none px-4 py-1 rounded-full shadow-lg bg-pink-300 text-black'>Spring</button>
